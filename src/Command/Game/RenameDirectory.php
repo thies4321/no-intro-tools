781a9159c11f5fd61f84067da959034e80200874
Thies Verhave <tverhave@transip.nl>
2022-03-21 13:56:03 +0100

    Add command to rename files within directory

---
+++ src/Command/Game/RenameDirectory.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoIntro\Command\Game;
+
+use NoIntro\Exception\InvalidPath;
+use NoIntro\Repository\GameRepository;
+use NoIntro\Service\GameService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use function sleep;
+use function sprintf;
+use function ucfirst;
+
+#[AsCommand(
+    name: 'game:rename:directory',
+    description: 'Find and replace files to match database entries in directory',
+    hidden: false
+)]
+final class RenameDirectory extends GameCommand
+{
+    private GameService $gameService;
+
+    public function __construct(
+        GameService $gameService = null,
+        GameRepository $gameRepository = null
+    ) {
+        $this->gameService = $gameService ?? new GameService();
+
+        parent::__construct($gameRepository);
+    }
+
+    public function configure(): void
+    {
+        $this->addArgument('path', InputArgument::REQUIRED, 'Directory to find files and rename');
+        $this->addArgument('commit', InputArgument::OPTIONAL, 'Commit renaming files', false);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $path = $input->getArgument('path');
+        $commit = (bool) $input->getArgument('commit');
+
+        if ($commit === true) {
+            $output->writeln('<info>Commit supplied. Changes WILL apply. 5 seconds to cancel...</info>');
+            sleep(5);
+        }
+
+        try {
+            $result = $this->gameService->organiseDirectory($path, $commit);
+        } catch (InvalidPath $exception) {
+            $output->writeln(sprintf('<error>[%s] is not a valid directory</error>', $path));
+            return Command::FAILURE;
+        }
+
+        foreach ($result as $status => $files) {
+            if (!empty($files)) {
+                $output->writeln(sprintf('%s:', ucfirst($status)));
+
+                foreach ($files as $file) {
+                    $output->writeln($file);
+                }
+
+                echo PHP_EOL;
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
--- src/Service/GameService.php
+++ src/Service/GameService.php
@@ -5,30 +5,75 @@ declare(strict_types=1);
 namespace NoIntro\Service;
 
 use NoIntro\Exception\InvalidPath;
+use NoIntro\Model\Game;
 use NoIntro\Repository\GameRepository;
+use function dirname;
+use function in_array;
 use function is_dir;
+use function rename;
 use function scandir;
+use function sha1_file;
+use function sprintf;
 
 final class GameService
 {
     private GameRepository $gameRepository;
 
-    public function __construct(GameRepository $gameRepository)
+    public function __construct(GameRepository $gameRepository = null)
     {
         $this->gameRepository = $gameRepository ?? new GameRepository\GameRepository();
     }
 
-    public function organiseDirectory(string $path): array
+    /**
+     * @throws InvalidPath
+     */
+    public function organiseDirectory(string $path, bool $commit = false): array
     {
         if (! is_dir($path)) {
             throw InvalidPath::forDirectory($path);
         }
 
         $files = scandir($path);
+
+        $result = [
+            'succes' => [],
+            'failed' => []
+        ];
+
+        foreach ($files as $file) {
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
+
+            $succes = $this->renameFile(sprintf('%s/%s', $path, $file), $commit);
+
+            if ($succes === true) {
+                $result['succes'][] = $file;
+                continue;
+            }
+
+            $result['failed'][] = $file;
+        }
+
+        return $result;
     }
 
-    public function renameFile(string $path): bool
+    public function renameFile(string $path, bool $commit = false): bool
     {
+        $sha1 = sha1_file($path);
 
+        $game = $this->gameRepository->getBySha1($sha1);
+
+        if (! $game instanceof Game) {
+            return false;
+        }
+
+        $dirName = dirname($path);
+
+        if ($commit === true) {
+            rename($path, sprintf('%s/%s', $dirName, $game->getRom()->getName()));
+        }
+
+        return true;
     }
 }
